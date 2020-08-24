9e2968fb5932dcd1c29c9ec77a26624269ca38cd
Maksim Lin <maks@manichord.com>
2020-08-24 16:36:54 +1000

    don't show keyguard aka lockscreen

--- android/app/src/main/kotlin/com/example/music_home/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/music_home/MainActivity.kt
@@ -1,6 +1,16 @@
 package com.example.music_home
 
+import android.app.KeyguardManager
+import android.content.Context
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val manager = this.getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+        val lock = manager.newKeyguardLock("music_home")
+        lock.disableKeyguard()
+    }
 }
