5d81b3a00f51bd16308c447c5bd6b9a416f19954
Maksim Lin <maks@manichord.com>
2020-08-24 21:03:20 +1000

    hide keyguard on newer android versions

--- android/app/src/main/kotlin/com/example/music_home/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/music_home/MainActivity.kt
@@ -5,12 +5,19 @@ import android.content.Context
 import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
+
 class MainActivity: FlutterActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val manager = this.getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
-        val lock = manager.newKeyguardLock("music_home")
-        lock.disableKeyguard()
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O_MR1) {
+            setShowWhenLocked(true)
+            manager.requestDismissKeyguard(this, null)
+        } else {
+            val lock = manager.newKeyguardLock("music_home")
+            lock.disableKeyguard()
+        }
     }
 }
+
